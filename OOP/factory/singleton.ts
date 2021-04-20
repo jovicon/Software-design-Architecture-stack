3e786742eae4204c962875982e7c2582b23d1971
Jose Contreras <jocontrera16@gmail.com>
2021-04-19 23:02:48 -0400

    adding singleton oop pattern

---
+++ OOP/factory/singleton.ts
@@ -0,0 +1,46 @@
+// Single pattern
+class Singleton {
+    private static instance: Singleton;
+    private static someVaribleA: number;
+    private static someVaribleB: number;
+
+    private constructor() {
+
+    }
+
+    public static getInstance(): Singleton {
+        if (!this.instance) {
+            this.instance = new Singleton();
+            this.someVaribleA = 3;
+            this.someVaribleB = 4;
+        }
+
+        return this.instance;
+    }
+
+    public businessLogicA() {
+        // Example...
+        return Singleton.someVaribleA * 3;
+    }
+
+    public businessLogicB() {
+        // Example...
+        return Singleton.someVaribleB - 3;
+    }
+}
+
+/**
+ * The client code.
+ */
+ function clientRunner() {
+    const singletonOne = Singleton.getInstance();
+    const singletonTwo = Singleton.getInstance();
+
+    if (singletonOne === singletonTwo) {
+        console.log('Singleton works, both variables contain the same instance.');
+    } else {
+        console.log('Singleton failed, variables contain different instances.');
+    }
+}
+
+clientRunner();
